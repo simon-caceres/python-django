94c8f1e8762a13e7071c1efc222f5ef5cbab6322
Simon Caceres <simon.caceres@egt.cl>
2021-06-04 18:17:12 -0400

    creando y migrando modelos

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    #User Model.
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length= 30)
+    first_name = models.CharField(max_length= 30)
+    last_name = models.CharField(max_length= 30)
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
