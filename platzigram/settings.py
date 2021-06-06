df20a365fa814811ebcfd718df7c0843b9322ed8
Simon Caceres <simon.caceres@egt.cl>
2021-06-05 23:22:57 -0400

    agregando

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', '../.env'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0@bp7yh$4(kh789r_e)&ulix#is_-h@ok6me-*39sjuni(gq&c'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
