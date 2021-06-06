d107478732f7aefc770a082dcc7a279ddaba3ac4
Simon Caceres <simon.caceres@egt.cl>
2021-06-05 23:12:35 -0400

    cambios en develop

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 # Django
-from django.contrib import admin 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+#Django models
+from django.contrib.auth.models import User
 
 #models personals
 from users.models import Profile
@@ -20,6 +23,7 @@ class ProfileAdmin(admin.ModelAdmin):
     )
     list_display_links = ('id','user')
     list_editable = ('phone_number', 'website', 'picture')
+    readonly_fields=('created','modified','user')
     search_fields = (
         'user__email',
         'user__username', 
@@ -48,4 +52,30 @@ class ProfileAdmin(admin.ModelAdmin):
                 ('biography')
             ),
         }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+            ),
+        }),
     )
+
+class ProfileInline(admin.StackedInline):
+    """ Profile in line admin for users """
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """add profile admin to base user admin """
+    inlines = (ProfileInline,)
+    list_display= (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
